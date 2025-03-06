fdc1ecc6e5ef94982d7c116b775a5b3236e23a7d
Joscha Feth <joscha@feth.com>
2025-03-06 12:57:31 +0100

    chore: use OOTB git hook

--- devenv.nix
+++ devenv.nix
@@ -10,16 +10,9 @@
   # https://devenv.sh/packages/
   packages = [
     pkgs.git
-    pkgs.python312Packages.openapi-spec-validator
   ];
 
-  # https://devenv.sh/tests/
-  enterTest = ''
-    openapi-spec-validator spec/*.yaml
-  '';
-
   scripts.format.exec = ''
-    yamlfmt .
     markdownlint --fix .
     pre-commit run --all-files
   '';
@@ -29,31 +22,11 @@
     typos.enable = true;
     yamllint.enable = true;
     yamlfmt.enable = true;
+    yamlfmt.settings.lint-only = false;
     commitizen.enable = true;
     nixfmt-rfc-style.enable = true;
     markdownlint.enable = true;
-  };
-
-  git-hooks.hooks.validate-spec = {
-    enable = true;
-
-    # The name of the hook (appears on the report table):
-    name = "Validate OpenAPI spec";
-
-    # The command to execute (mandatory):
-    entry = "openapi-spec-validator";
-
-    # The pattern of files to run on (default: "" (all))
-    # see also https://pre-commit.com/#hooks-files
-    files = "^spec/.*\\.(yaml|yml|json)$";
-
-    # The language of the hook - tells pre-commit
-    # how to install the hook (default: "system")
-    # see also https://pre-commit.com/#supported-languages
-    language = "system";
-
-    # Set this to false to not pass the changed files
-    # to the command (default: true):
-    pass_filenames = true;
+    mdformat.enable = true;
+    openapi-spec-validator.enable = true;
   };
 }
